73c0ea1b0060e0fced342b6ff70d803fdc431603
yangsenit <864062489@qq.com>
2016-07-16 16:42:30 +0800

    1
    2

--- src/test/scala/text1.scala
+++ src/test/scala/text1.scala
@@ -1,6 +1,15 @@
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
   * Created by ys on 16-7-16.
   */
 object text1 {
-
+  def main(args: Array[String]): Unit = {
+    val conf=new SparkConf().setAppName("text1").setMaster("local[*]")
+    val sc=new SparkContext(conf)
+    val hiveContext=new HiveContext(sc)
+    val rdd1=sc.textFile("G:\\workstation\\SparkDemo\\data\\1.txt").map(x=>{x.trim.split(",")})
+    rdd1.foreach(print _)
+  }
 }
--- src/test/scala/text2.scala
+++
@@ -1,6 +0,0 @@
-/**
-  * Created by ys on 16-7-16.
-  */
-object text2 {
-
-}
